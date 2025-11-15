f8ed7d928de9ec48a6e38c015d7ea006cabb53a7
Sushant  Gaurav <156192797+9sushant@users.noreply.github.com>
2025-11-15 13:06:16 +0530

    Update index.tsx

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
-
+ 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
 
