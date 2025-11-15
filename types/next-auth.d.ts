da1bc7c9b140cbebf74bf91179e795e36ad81a0e
Sushant  Gaurav <156192797+9sushant@users.noreply.github.com>
2025-11-15 12:52:18 +0530

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,12 +2,12 @@ import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      role: string;
+    user?: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string;  // <-- IMPORTANT
     } & DefaultSession["user"];
-  }
-
-  interface User {
-    role: string;
   }
 }
