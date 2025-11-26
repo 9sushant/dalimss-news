5bd7d83ae44f8aff07a6901d8792ec7c190b5f12
9sushant <sushantgaurav80@gmail.com>
2025-11-26 11:25:10 +0530

    Reduce gap between nav and hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function HomePage({ articles }: Props) {
   const sidebarNews = articles.slice(2, 8); // Just reusing for demo
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 pb-6 pt-2">
         
         {/* HERO SECTION */}
         <section className="mb-12">
