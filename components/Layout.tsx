6844a4ee03ac0f1626455ef61880d97c4d136926
9sushant <sushantgaurav80@gmail.com>
2025-12-30 15:46:27 +0530

    feat: set favicon to site logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,8 +5,15 @@ interface LayoutProps {
   children: ReactNode;
 }
 
+import Head from "next/head";
+
 const Layout = ({ children }: LayoutProps) => (
   <div className="bg-white min-h-screen text-gray-900 font-sans">
+    <Head>
+      <title>Dalimss News</title>
+      <meta name="description" content="Dalimss News - Latest Updates" />
+      <link rel="icon" href="/logo.png" />
+    </Head>
     <Nav />
     <main>{children}</main>
   </div>
